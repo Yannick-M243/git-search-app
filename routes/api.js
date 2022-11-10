76b0f1424a77dd36d7cb19f607e77fc28eef0579
Yannick Makwenge <yannickmakwenge@gmail.com>
2022-11-10 21:52:50 +0200

    Finalising first version

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const useragent = require('express-useragent');
 router.use(useragent.express());
 
 //endpoint to fetch information from github users
-router.get('/github/userinfo/:user', async function (req, res) {
+router.get('/github/userinfo/:user', function (req, res) {
     const user = req.params.user;
     const options = {
         hostname: 'api.github.com',
@@ -24,12 +24,11 @@ router.get('/github/userinfo/:user', async function (req, res) {
 });
 
 //endpoint to fetch information from github repositories
-router.get('/github/repoinfo/:user/:repo', async function (req, res) {
+router.get('/github/repoinfo/:user', function (req, res) {
     const user = req.params.user;
-    const reponame = req.params.repo; 
     const options = {
         hostname: 'api.github.com',
-        path: '/repos/' + user +'/'+ reponame,
+        path: '/users/' + user +'/repos',
         headers: {
             'User-Agent': req.useragent
         },
@@ -43,13 +42,13 @@ router.get('/github/repoinfo/:user/:repo', async function (req, res) {
     })
 });
 
-//endpoint to fetch information from github commits
-router.get('/github/commitsinfo/:user/:repo', async function (req, res) {
+//endpoint to fetch information from github events
+router.get('/github/eventsinfo/:user', function (req, res) {
     const user = req.params.user;
     const reponame = req.params.repo;
     const options = {
         hostname: 'api.github.com',
-        path: '/repos/' + user + '/' + reponame + '/commits',
+        path: '/users/' + user + '/events',
         headers: {
             'User-Agent': req.useragent
         },
@@ -64,31 +63,11 @@ router.get('/github/commitsinfo/:user/:repo', async function (req, res) {
 });
 
 //endpoint to fetch information from gitlab users
-router.get('/gitlab/userinfo/:user', async function (req, res) {
+router.get('/gitlab/userinfo/:user', function (req, res) {
     const user = req.params.user;
     const options = {
         hostname: 'gitlab.com',
         path: '/api/v4/users?username=' + user,
-        headers: {
-            'User-Agent': req.useragent
-        },
-        OAUth: process.env.GITLAB_API_TOKEN
-    };
-    https.get(options, function (apiResponse) {
-        apiResponse.pipe(res);
-    }).on('error', (e) => {
-        console.log(e);
-        res.status(500).send('Something went wrong!');
-    })
-});
-
-//endpoint to fetch information from gitlab repository
-router.get('/gitlab/repoinfo/projects', async function (req, res) {
-    //const id = req.params.id;
-    const options = {
-        hostname: 'gitlab.com',
-        //path: '/api/v4/projects/'+id+'/repository/tree',
-        path: '/api/v4/projects',
         headers: {
             'User-Agent': req.useragent
         },
